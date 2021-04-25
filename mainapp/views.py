004b4e387290f8c928cb7c99f321bc6db9a52cef
Kllraz <kllraz@yandex.ru>
2021-04-25 12:07:02 +0300

    Lesson 2

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
+import json
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'title': 'GeekShop',
+    }
+    return render(request, 'mainapp/index.html', context)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    with open('mainapp/fixtures/products.json', 'r', encoding='utf-8') as file:
+        decode_products = json.load(file, )
+
+    context = {
+        'title': 'GeekShop - Каталог',
+        'products': decode_products
+    }
+    return render(request, 'mainapp/products.html', context)
