8f57ea20ada95dfdd1526951c866d7596e9be602
Kllraz <kllraz@yandex.ru>
2021-05-01 16:02:23 +0300

    add styles for forms

--- authapp/views.py
+++ authapp/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
+from authapp.forms import LoginForm, RegisterForm
 
 
 # Create your views here.
 
 def login(request):
+    form = LoginForm()
+
     context = {
         'title': 'GeekShop - Авторизация',
+        'form': form,
     }
     return render(request, 'authapp/login.html', context)
 
 
 def register(request):
+    form = RegisterForm()
+
     context = {
         'title': 'GeekShop - Регистрация',
+        'form': form,
     }
     return render(request, 'authapp/register.html', context)
