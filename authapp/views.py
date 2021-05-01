a5d2e044c3fcd90f917d443c6bdd404be121353b
Kllraz <kllraz@yandex.ru>
2021-05-01 21:06:26 +0300

    change controller for auth

--- authapp/views.py
+++ authapp/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from authapp.forms import LoginForm, RegisterForm
+from django.contrib import auth
 
 
 # Create your views here.
 
 def login(request):
+    if request.method == 'POST':
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+
+            if user and user.is_active:
+                auth.login(request, user)
+                return redirect('index')
     form = LoginForm()
 
     context = {
@@ -15,10 +26,20 @@ def login(request):
 
 
 def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('auth:login')
     form = RegisterForm()
-
     context = {
         'title': 'GeekShop - Регистрация',
         'form': form,
     }
     return render(request, 'authapp/register.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+
+    return redirect('index')
