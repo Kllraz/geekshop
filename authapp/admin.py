356ca109d3ad84213cf6f75c481055e93430bcc8
Kllraz <kllraz@yandex.ru>
2021-05-19 19:55:28 +0300

    edit django admin panel for user

--- authapp/admin.py
+++ authapp/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from authapp.models import User
 
+
 # Register your models here.
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'email')
+    readonly_fields = ('password', 'date_joined', 'last_login')
+    search_fields = ('username', 'first_name', 'last_name')
+    ordering = ('username', 'first_name', 'last_name')
