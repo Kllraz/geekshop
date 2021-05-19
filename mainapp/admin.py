674d6bb45e3d534c14fc6bbd3133d5a779765eed
Kllraz <kllraz@yandex.ru>
2021-05-19 20:06:58 +0300

    edit django admin panel for product category

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from mainapp.models import ProductCategory, Product
 
-# Register your models here.
 
-admin.site.register(ProductCategory)
+# Register your models here.
 
 
 @admin.register(Product)
@@ -12,3 +11,9 @@ class ProductAdmin(admin.ModelAdmin):
     search_fields = ('name', 'category__name')
     ordering = ('quantity', 'price')
     fields = ('name', 'description', 'category', 'image', ('price', 'quantity'))
+
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    ordering = ('name',)
